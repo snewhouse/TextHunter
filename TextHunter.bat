7709f52e77a3edb9855c693434d5919764f09f73
RichJackson <RichJackson@users.noreply.github.com>
2014-06-03 16:47:49 +0100

    Update TextHunter.bat

--- TextHunter.bat
+++ TextHunter.bat
@@ -1,9 +1,4 @@
 REM @echo off
-set JAVA_HOME="\\achlys\shared\BRC_CRIS\apps\java\jdk1.7.0_25"
-
-
-
-
-%JAVA_HOME%\bin\java  -Djava.library.path="T:\apps\jtds-1.3.1-dist\x86\SSO" -Dgate.home=\\achlys\shared\BRC_CRIS\GATE\gate-7.1-build4485 -cp %JAVA_HOME%\lib\*; -jar "C:\Users\rjackson1\Documents\NetBeansProjects\TextHunter\dist\TextHunter.jar" 
+java  -Djava.library.path="jtds-1.3.1-dist\x86\SSO" -jar "dist\TextHunter.jar" 
 
 pause
